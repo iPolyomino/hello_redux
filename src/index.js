123b1eb4e377f7c875614184af86950cfa830009
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2018-05-15 21:51:41 +0900

    todo

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import { createStore } from 'redux'
-import Counter from './components/Counter'
-import counter from './reducers'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import rootReducer from './reducers'
 
-const store = createStore(counter)
-const rootEl = document.getElementById('root')
+const store = createStore(rootReducer)
 
-const render = () => ReactDOM.render(
-  <Counter
-    value={store.getState()}
-    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-  />,
-  rootEl
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 )
-
-render()
-store.subscribe(render)
